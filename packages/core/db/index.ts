6cc9887bcd3e8d871ab5f72ca717a452e286d360
Jiyu Park <me@jiyu.land>
2026-05-06 19:22:22 +0900

    DB 관계 스키마 메모를 통합한다

--- packages/core/db/index.ts
+++ packages/core/db/index.ts
@@ -2,7 +2,6 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import * as enums from './enums';
 import { relations } from './relations';
 import * as tables from './tables';
-import type { PgDatabase, PgTransaction } from 'drizzle-orm/pg-core';
 
 export * from './id';
 export * from './relations';
@@ -15,5 +14,3 @@ export const db = drizzle(process.env.DATABASE_URL!, {
 });
 
 export type Database = typeof db;
-export type Transaction =
-  Database extends PgDatabase<infer T, infer U, infer V> ? PgTransaction<T, U, V> : never;
