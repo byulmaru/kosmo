8bd1464a092097d6d31bbe4b779cac1e44f933c4
Jiyu Park <me@jiyu.land>
2026-05-21 16:09:44 +0900

    세션 노드 로더를 추가한다

--- apps/api/src/graphql/resolvers/session/ref.ts
+++ apps/api/src/graphql/resolvers/session/ref.ts
@@ -1,7 +1,10 @@
-import { Sessions, TableDiscriminator } from '@kosmo/core/db';
+import { db, Sessions, TableDiscriminator } from '@kosmo/core/db';
+import { inArray } from 'drizzle-orm';
 import { createObjectRef } from '@/graphql/utils';
 
-export const Session = createObjectRef('Session', Sessions, TableDiscriminator.Sessions);
+export const Session = createObjectRef('Session', Sessions, TableDiscriminator.Sessions, (ids) =>
+  db.select().from(Sessions).where(inArray(Sessions.id, ids)),
+);
 
 Session.implement({
   authScopes: (session, ctx) => session.id === ctx.session?.id,
