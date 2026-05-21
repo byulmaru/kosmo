7da6dae1c6e01d48aff02bf098b947c2b1347016
Jiyu Park <me@jiyu.land>
2026-05-21 16:16:26 +0900

    세션 노드 ref 인자를 단순화한다

--- apps/api/src/graphql/resolvers/session/ref.ts
+++ apps/api/src/graphql/resolvers/session/ref.ts
@@ -2,7 +2,7 @@ import { db, Sessions, TableDiscriminator } from '@kosmo/core/db';
 import { inArray } from 'drizzle-orm';
 import { createObjectRef } from '@/graphql/utils';
 
-export const Session = createObjectRef('Session', Sessions, TableDiscriminator.Sessions, (ids) =>
+export const Session = createObjectRef('Session', TableDiscriminator.Sessions, (ids) =>
   db.select().from(Sessions).where(inArray(Sessions.id, ids)),
 );
 
