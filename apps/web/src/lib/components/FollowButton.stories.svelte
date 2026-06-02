6611a43516db7b8486b49e001c7b7db09b97a8bf
HJSmiley <hyeju518@gmail.com>
2026-06-02 17:59:48 +0900

    팔로우 버튼 스토리 설명 텍스트 제거

--- apps/web/src/lib/components/FollowButton.stories.svelte
+++ apps/web/src/lib/components/FollowButton.stories.svelte
@@ -79,15 +79,6 @@
       {followAction}
       {unfollowAction}
     />
-    <div class="flex min-h-8 items-center gap-3">
-      <FollowButtonView
-        targetProfileId={viewerProfileId}
-        {viewerProfileId}
-        {followAction}
-        {unfollowAction}
-      />
-      <span class="text-text-secondary text-xs">본인 프로필에서는 버튼을 표시하지 않음</span>
-    </div>
     <FollowButtonView {targetProfileId} viewerProfileId={null} {followAction} {unfollowAction} />
     <FollowButtonView
       {targetProfileId}
