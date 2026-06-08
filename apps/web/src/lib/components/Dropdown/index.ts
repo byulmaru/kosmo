ada585b9f8eb7f601b3112911d3770474667910e
Jiyu Park <jiyu@byulmaru.co>
2026-06-08 16:48:39 +0900

    PROD-87 Dropdown primitive 추가

---
+++ apps/web/src/lib/components/Dropdown/index.ts
@@ -0,0 +1,7 @@
+import { DropdownMenu as DropdownMentPrimitive } from 'bits-ui';
+import Content from './Content.svelte';
+import Item from './Item.svelte';
+import Trigger from './Trigger.svelte';
+
+const Root = DropdownMentPrimitive.Root;
+export { Content, Item, Root, Trigger };
