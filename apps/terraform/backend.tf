dc7326e68e8bb315f4ac237ab7632a6645d315ca
Jiyu Park <jiyu@byulmaru.co>
2026-07-12 23:18:01 +0900

    Terraform state key를 일반화한다

--- apps/terraform/backend.tf
+++ apps/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "byulmaru-terraform-state"
-    key          = "kosmo/firebase.tfstate"
+    key          = "kosmo/terraform.tfstate"
     region       = "ap-northeast-2"
     use_lockfile = true
   }
