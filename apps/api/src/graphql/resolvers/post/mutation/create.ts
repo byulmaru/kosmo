41a994d5408b237e605a959afece58d6ff26eedb
Jiyu Park <jiyu@byulmaru.co>
2026-07-15 17:47:18 +0900

    PROD-341 createPost 중복 검증을 제거한다

--- apps/api/src/graphql/resolvers/post/mutation/create.ts
+++ apps/api/src/graphql/resolvers/post/mutation/create.ts
@@ -1,9 +1,6 @@
 import { db, firstOrThrow, PostContents, Posts } from '@kosmo/core/db';
 import { PostState, PostVisibility } from '@kosmo/core/enums';
-import {
-  postContentDocumentFromText,
-  validateLocalPostContentDocument,
-} from '@kosmo/core/post-content/server';
+import { postContentDocumentFromText } from '@kosmo/core/post-content/server';
 import { postBodyTextSchema } from '@kosmo/core/validation';
 import { eq } from 'drizzle-orm';
 import { builder } from '@/graphql/builder';
@@ -21,7 +18,7 @@ builder.mutationField('createPost', (t) =>
       visibility: t.input.field({ type: PostVisibility }),
     },
     resolve: async (_, { input }, ctx) => {
-      const body = validateLocalPostContentDocument(postContentDocumentFromText(input.bodyText));
+      const body = postContentDocumentFromText(input.bodyText);
       const post = await db.transaction(async (tx) => {
         const post = await tx
           .insert(Posts)
