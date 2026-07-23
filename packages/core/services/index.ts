edb05a4aa34b142be9807c54f5088d8ea845440c
HJSmiley <hyeju518@gmail.com>
2026-07-24 08:33:50 +0900

    fix: remove duplicate service exports in index

--- packages/core/services/index.ts
+++ packages/core/services/index.ts
@@ -5,7 +5,6 @@ export {
   deleteNotificationBySource,
 } from './notification';
 export { createPost, deletePost, repostPost } from './post';
-export { createPost, deletePost, repostPost } from './post';
 export { disableProfile } from './profile';
 export { followProfile, removeInboundFollow, unfollowProfile } from './profile-follow';
 export {
