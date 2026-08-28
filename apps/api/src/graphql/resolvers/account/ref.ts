893d013cbcd001717a8ae373750b1c082f7be46f
Jiyu Park <jiyu@byulmaru.co>
2026-08-28 17:40:05 +0900

    Account 피처 플래그 필드 노출을 단순화

--- apps/api/src/graphql/resolvers/account/ref.ts
+++ apps/api/src/graphql/resolvers/account/ref.ts
@@ -16,9 +16,7 @@ export const Account = createObjectRef('Account', (ids, ctx) =>
 
 Account.implement({
   fields: (t) => ({
-    featureFlags: t.stringList({
-      resolve: (account) => account.featureFlags,
-    }),
+    featureFlags: t.exposeStringList('featureFlags'),
     name: t.string({
       resolve: (account) => account.displayName,
     }),
