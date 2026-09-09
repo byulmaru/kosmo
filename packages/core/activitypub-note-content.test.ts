4c6326fdf382a57d169956a3ca87e1ad5bc0cd79
HJSmiley <hyeju518@gmail.com>
2026-09-09 18:15:28 +0900

    PROD-465 테스트 import 정렬을 수정한다

--- packages/core/activitypub-note-content.test.ts
+++ packages/core/activitypub-note-content.test.ts
@@ -1,9 +1,9 @@
 import assert from 'node:assert/strict';
 import { describe, it } from 'node:test';
 import {
-  RemoteNoteContentLengthExceededError,
   projectRemoteActivityPubHtmlToPlainText,
   projectRemoteNoteContent,
+  RemoteNoteContentLengthExceededError,
 } from './activitypub-note-content';
 
 describe('projectRemoteNoteContent', () => {
