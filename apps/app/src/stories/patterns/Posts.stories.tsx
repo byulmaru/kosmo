8a6a5563a244d2a33238b581c790f6c220cea970
Jiyu Park <jiyu@byulmaru.co>
2026-09-14 14:33:02 +0900

    test: PostContent fixture에 Mention 관계를 추가한다

--- apps/app/src/stories/patterns/Posts.stories.tsx
+++ apps/app/src/stories/patterns/Posts.stories.tsx
@@ -2698,6 +2698,7 @@ function PostMediaViewerRevisionStory() {
               );
               contentRecord.setValue('content-post-media-viewer-quote-revision', 'id');
               contentRecord.setValue(longBody, 'bodyText');
+              contentRecord.setLinkedRecords([], 'mentionedProfiles');
               contentRecord.setLinkedRecords(
                 originalContent.media!.map((media, index) => {
                   const mediaRecord = store.create(`viewer-revision-media-${index}`, 'Media');
